e2ca8cd54fd028da9fcb143d364d3a3e40be8b17
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-23 17:39:04 +0100

    hero inclusdes

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,21 @@
+<header class="container">
+    <figure id="logo-header">
+        <a href="#">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC_Logo">
+        </a>
+    </figure>
+    <nav>
+        <ul>
+            <li>
+                <a :href="">Home</a>
+                <a :href="">Home</a>
+                <a :href="">ciao</a>
+                <a :href="">Home</a>
+                <a :href="">Home</a>
+                <a :href="">Home</a>
+                <a :href="">ciao</a>
+                <a :href="">Home</a>
+            </li>
+        </ul>
+    </nav>
+</header>
---
+++ resources/views/includes/hero.blade.php
@@ -0,0 +1,25 @@
+<div class="image-main">
+    <img src="../assets/img/jumbotron.jpg" alt="Teen Titans">
+</div>
+
+<!-- BLACKBOARD PRODUCTS LIST -->
+<section id="contents-space">
+    <div class="blackboard">
+        
+    </div>
+</section>
+
+<!-- CENTER NAV -->
+<section id="center-nav">
+    <nav class="container">
+        <!-- LINK LIST -->
+        <ul>
+            <li>
+                <a href="">
+                    <img :src="" alt="">
+                    {{-- {{ jumboimage.text }} --}}
+                </a>
+            </li>
+        </ul>
+    </nav>
+</section>
